2227d89bd6ae7c451d1fc92f67e6c05dcbd5d93d
Neelaksh <neelaksh48@gmail.com>
2021-08-07 13:59:34 +0530

    final dest

---
+++ busdata1.py
@@ -0,0 +1,73 @@
+from pykafka import KafkaClient
+import json
+from datetime import date, datetime
+import uuid
+import time
+
+# Basic producer configurstion
+client = KafkaClient(hosts="localhost:9092")
+
+topic = client.topics['geodata_final']
+
+producer = topic.get_sync_producer()
+
+# Read coordinates from GEOJSON
+input_file1 = open('./data/bus1.json')
+json_array = json.load(input_file1)
+
+coordinates = json_array['features'][0]['geometry']['coordinates']
+# print(coordinates)
+
+# Generate UUID
+
+
+def generate_uuid():
+    return uuid.uuid4()
+
+
+data = {}
+data['busline'] = '00001'
+
+# Construct message and send it to kafka
+
+
+def generate_checkpoint(coordinates):
+    i = 0
+    while i < len(coordinates):
+        data['key'] = data['busline'] + '_' + str(generate_uuid())
+        data['timestamp'] = str(datetime.utcnow())
+        data['latitude'] = coordinates[i][1]
+        data['longitude'] = coordinates[i][0]
+        message = json.dumps(data)
+        # print(message)
+        producer.produce(message.encode('ascii'))
+        time.sleep(1)
+        # if bus reaches the end it starts to loop again
+        if i == len(coordinates)-1:
+            i = 0
+        else:
+            i += 1
+
+
+generate_checkpoint(coordinates)
+
+# print(data)
+
+
+'''
+client = KafkaClient(hosts="localhost:9092")
+
+topic = client.topics['testBusdata']
+
+producer = topic.get_sync_producer()
+
+count = 1
+while count < 11:
+    message = ("hello-" + str(count)).encode('ascii')
+    producer.produce(message)
+    count = count+1
+
+'''
+
+
+# testBusdata
